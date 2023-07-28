c431325600cb56155a0398ca6065dee89351c16d
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2023-07-28 20:29:29 +0300

    Add localStorage data-base

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,13 +1,21 @@
-import {tasksReducer} from './tasks/tasks-reducer';
-import {todolistsReducer} from './todolists/todolists-reducer';
-import {combineReducers, legacy_createStore} from 'redux';
-
+import { tasksReducer } from "./tasks/tasks-reducer";
+import { todolistsReducer } from "./todolists/todolists-reducer";
+import { combineReducers, legacy_createStore } from "redux";
 
 const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todolistsReducer,
-})
-export const store = legacy_createStore(rootReducer);
-export type AppRootStateType = ReturnType<typeof rootReducer>
+  tasks: tasksReducer,
+  todolists: todolistsReducer,
+});
 
+let preloadedState;
+const persistedTodosString = localStorage.getItem("app-state");
+if (persistedTodosString) {
+  preloadedState = JSON.parse(persistedTodosString);
+}
+export const store = legacy_createStore(rootReducer, preloadedState);
 
+store.subscribe(() => {
+  localStorage.setItem("app-state", JSON.stringify(store.getState()));
+});
+
+export type AppRootStateType = ReturnType<typeof rootReducer>;
